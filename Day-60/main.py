b90b8bd7d59bc2b8e8624641ae8a47f5ada6bd97
ravi0811 <rr957226@gmail.com>
2026-08-12 23:46:12 +0530

    Add files via upload

---
+++ Day-60/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template,request
+import requests
+
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET","POST"])
+def contact():
+    if request.method=="GET":
+        return render_template("contact.html",saved="False")
+    else:
+        name=request.form["name"]
+        email=request.form["email"]
+        phone=request.form["phone"]
+        message= request.form["message"]
+        return render_template("contact.html",saved="True")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
